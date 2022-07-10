d605b127680878b7c6940c6a18ef80a0868f6a26
Egehan Asal <egehanasal.se@gmail.com>
2022-07-10 18:51:57 +0300

    Added some features

--- java/JavaMaps/app/src/main/java/com/egehanasal/javamaps/MapsActivity.java
+++ java/JavaMaps/app/src/main/java/com/egehanasal/javamaps/MapsActivity.java
@@ -1,13 +1,22 @@
 package com.egehanasal.javamaps;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,11 +25,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.egehanasal.javamaps.databinding.ActivityMapsBinding;
+import com.google.android.material.snackbar.Snackbar;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    ActivityResultLauncher <String> permissionLauncher;
+
+    private static boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +45,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        registerLauncher();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -47,17 +66,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener locationListener = new LocationListener() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
+
+                // Uygulama başlarken konumumuzdan başlayacak, sonra kamerayı istediğimiz gibi dolaştırabileceğiz
+                if(MapsActivity.flag){
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15f));
+                    MapsActivity.flag = false;
+                }
                 System.out.println("location: "+ location.toString());
             }
         };
 
-        LatLng eiffel = new LatLng(48.8559713,2.2930037);
-        mMap.addMarker(new MarkerOptions().position(eiffel).title("Eiffel Tower"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eiffel, 10f));
+        // İzin vermemişse izin iste
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Kullanıcıya göstermemiz gerekiyorsa
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Snackbar.make(binding.getRoot(), "Permission needed for Maps", Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // İzin iste
+                        permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                    }
+                }).show();
+            }
+            else {
+                // Kullanıcıya göstermemiz gerekmiyorsa
+                permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+        }
+        // İzin verilmişse lokasyon al
+        else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            // Artık izin olduğuna eminiz,
+            mMap.setMyLocationEnabled(true);
+
+            /*
+            // Opsiyonel (Uygulama kullanılırkenki son konumdan başlayacak harita, kullanıcı offline olduğunda uygulamayı
+            // açarsa işe yarayabilecek bir feature)
+            Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastLocation != null) {
+                LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f));
+            }
+             */
+        }
+    }
+
+    private void registerLauncher() {
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                if(result) {
+                    // İzin verildi ama tekrar kontrol etmek gerekiyor
+                    if(ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0,locationListener);
+
+                        /*
+                        // Opsiyonel (Uygulama kullanılırkenki son konumdan başlayacak harita, kullanıcı offline olduğunda uygulamayı
+                        // açarsa işe yarayabilecek bir feature)
+                        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if(lastLocation != null) {
+                            LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation, 15f));
+                        }
+                         */
+                    }
+                }
+                else {
+                    // İzin verilmedi
+                    Toast.makeText(MapsActivity.this, "Permission needed!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+
+        // Önceden koyulan marker'ı kaldırıyor yeni marker koymak istediğimizde
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng));
     }
 }
